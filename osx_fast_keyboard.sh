43132a6526d664dfd3fd169abff711d7862b07da
Richard Tessier <rtessier@coveo.com>
2017-09-17 22:52:04 -0400

    * Set workable value for macOS sierra

--- osx_fast_keyboard.sh
+++ osx_fast_keyboard.sh
@@ -1,8 +1,9 @@
+# On macOS Sierra, a restart was required to get these settings to apply "right"
 echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0.02
+defaults write NSGlobalDomain KeyRepeat -int 1
 
 echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Revert to default settings with:
 # defaults delete NSGlobalDomain KeyRepeat
